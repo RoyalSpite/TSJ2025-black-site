9574d27042c46ec5d2f292a03cc4a9042c0cda6a
PuttNAJA <chayaphon.kid@gmail.com>
2025-04-17 11:17:51 +0700

    Enemy's Move & Destroy

--- test/Assets/Scripts/Enemy.cs
+++ test/Assets/Scripts/Enemy.cs
@@ -2,30 +2,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private readonly float YBorder = 5f;
-    private bool isUp = true;
-    [SerializeField] private float speed = 11f;
+    public float moveSpeed = 3f;
+    private Transform player;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
-        //Move
-        Vector3 newPos = new Vector3();
-
-        int dir = isUp? 1 : -1;
-        newPos.y = dir * (Time.deltaTime * speed);
-
-        if(Mathf.Abs(newPos.y + transform.position.y) >= YBorder){
-            isUp = !isUp;
-            transform.position = new Vector3(
-                transform.position.x,
-                dir * YBorder
-            );
-        }
-        else transform.position += newPos;
-
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
     }
 
+    void Update()
+    {
+        if (player != null)
+        {
+            // เดินตรงเข้าหา Player
+            Vector3 direction = (player.position - transform.position).normalized;
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
